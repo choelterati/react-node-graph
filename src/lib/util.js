a6017b34ddc5a2fd184e149fa0dba02a2a0849a1
Christopher Hoelter <choelter@gmrmarketing.com>
2017-04-27 15:28:23 -0500

    Update util.js
    Modifying connector spacing.

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,16 +1,16 @@
 /* not bound to style, should be computed */
 
 export function computeInOffsetByIndex(x,y,index) {
-	let outx = x + 15;
-	let outy = y + 47 + (index * 20);
+	let outx = x + 16.5;
+	let outy = y + 41 + (index * 20);
 
 	return {x:outx, y:outy};
 }
 
 export function computeOutOffsetByIndex(x,y,index) {
 
-	let outx = x + 166;
-	let outy = y + 49 + (index * 22);
+	let outx = x + 471.5;
+	let outy = y + 43 + (index * 22);
 
 	return {x:outx, y:outy};
 
